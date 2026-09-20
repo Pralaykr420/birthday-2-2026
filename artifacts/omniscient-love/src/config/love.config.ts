d638c756ededa5f59f09879679b65ab3e72aa1fb
Pralay <pralaykr20@gmail.com>
2026-09-20 17:27:00 +0530

    Test time-3

--- artifacts/omniscient-love/src/config/love.config.ts
+++ artifacts/omniscient-love/src/config/love.config.ts
@@ -51,7 +51,7 @@ export const loveConfig = {
      * takes over the screen with the midnight surprise.
      */
     surpriseHour: 17,
-    surpriseMinute: 22,
+    surpriseMinute: 28,
   },
 
   /** The day you two started. Used for the "days together" counter. */
